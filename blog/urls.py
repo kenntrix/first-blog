9fd2016ee19b1b8ac606c16932c834f33446d220
kennto <kenntrixwambuzz@gmail.com>
2024-05-12 12:41:37 +0300

    changed the HTML for the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path(', views.home', name='home'),
+    path('', views.home, name='home'),
+    path('hello/', views.post_list, name='hello'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('hello world')
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
 # Create your views here.
